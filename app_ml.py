f32546c32ecc312278540633a4cb69d148c286b7
WangHyunSung <120348500+hyunsungKR@users.noreply.github.com>
2023-05-09 23:39:33 +0900

    Update app_ml.py
    add

--- app_ml.py
+++ app_ml.py
@@ -1,3 +1,4 @@
+from app_eda import show_column_description
 import streamlit as st
 import numpy as np
 import joblib
@@ -34,17 +35,7 @@ def run_ml():
     st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ2hDyjLKlxs6o6VuayarJJ7V133ht5yZTsQ&usqp=CAU.jpg',width=350)
     if st.button('데이터 보기') :
         st.dataframe(df)
-        with st.expander('데이터프레임 컬럼 상세 설명') :
-            st.subheader('데이터프레임 컬럼 상세 설명')
-            st.text('GENERAL APPEARANCE : 일반 외관')
-            st.text('MANNER OF SPEAKING : 말하는 방식')
-            st.text('PHYSICAL CONDITION : 신체 조건')
-            st.text('MENTAL ALERTNESS : 정신적 경계')
-            st.text('SELF-CONFIDENCE : 자신감')
-            st.text('ABILITY TO PRESENT IDEAS : 아이디어 제시 능력')
-            st.text('COMMUNICATION SKILLS : 의사 소통 능력')
-            st.text('Student Performance Rating : 학생 수행 평가')
-            st.text('CLASS  : 고용 가능 0은 불가능 1은 가능')
+        show_column_description()
     score = ['매우안좋다','안좋다','보통이다','좋다','매우좋다']
     G_A=st.radio('본인의 일반외관 점수는 어떠합니까.',[score[0],score[1],score[2],score[3],score[4]])
     if G_A == score[0] :
